306aaf395d79f065acd259fdddcfb6f187fa0e60
Enrico Zimuel <e.zimuel@gmail.com>
2016-05-04 18:55:39 +0200

    Added the list of packages in learn page

--- src/App/Action/LearnAction.php
+++ src/App/Action/LearnAction.php
@@ -19,7 +19,8 @@ class LearnAction
         $page = $request->getAttribute('page', false);
 
         if (false === $page) {
-            return new HtmlResponse($this->template->render("app::learn"));
+            $components = json_decode(file_get_contents('http://zendframework.github.io/zf-mkdoc-theme/scripts/zf-component-list.json'));
+            return new HtmlResponse($this->template->render("app::learn", [ 'components' => $components ]));
         }
 
         if (! in_array($page, [ 'learn', 'training-and-certification', 'support-and-consulting' ])) {
