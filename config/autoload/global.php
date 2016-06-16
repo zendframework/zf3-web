5d00802fccb3285c407889b33a8d09e07b792502
Enrico Zimuel <e.zimuel@gmail.com>
2016-06-16 07:54:22 +0200

    Fixed the footer + global config

---
+++ config/autoload/global.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'debug' => false,
+    'zf_manual_basepath' => '/mnt/efs/manual',
+    'config_cache_enabled' => true,
+    'cache_dir' => '/mnt/local/cache/framework.zend.com/'
+];
