e73db5d0bea0dd8e65c76b0f6e89cb9caada1a23
webimpress <contact@webimpress.com>
2017-09-21 19:14:40 +0100

    Added npm+gulp assets manager
    - Updated Bootstrap and jQuery - linked from CDN servers
    - Removed old unused libraries
    - Added npm and gulp to build resources for the website
    - Used 'asset' view helper from zend-view 2.9.0
    - Added section in README.md how to rebuild assets

--- README.md
+++ README.md
@@ -142,3 +142,19 @@ environment variable before invoking the script:
 ```bash
 $ APP_CACHE=/path/to/cache/ php bin/build.php
 ```
+
+## CSS and JavaScript
+
+CSS can be found in the `asset/sass/` directory (we use SASS for defining our CSS),
+and JS can be found in the `asset/js/` directory.
+
+After changing CSS or JS you need rebuild assets, as following:
+```bash
+$ cd asset
+$ npm install
+$ gulp
+```
+
+New files will be generated in `public/js` and `public/css` and old files will
+be removed. File `asset/rev-manifest.json` will contain new revision names for
+our assets. The file is used by [`asset` view helper](https://docs.zendframework.com/zend-view/helpers/asset/).
---
+++ asset/gulpfile.js
@@ -0,0 +1,98 @@
+require('es6-promise').polyfill();
+
+var gulp = require('gulp'),
+    cssimport = require('gulp-cssimport'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rev = require('gulp-rev'),
+    revDel = require('rev-del'),
+    revOriginDel = require('gulp-rev-delete-original'),
+    prism = [
+        'core',
+        'markup',
+        'css',
+        'clike',
+        'javascript',
+        'apacheconf',
+        'bash',
+        'batch',
+        'css-extras',
+        'diff',
+        'docker',
+        'git',
+        'handlebars',
+        'http',
+        'ini',
+        'json',
+        'less',
+        'makefile',
+        'markdown',
+        'nginx',
+        'php',
+        'php-extrasx',
+        'powershell',
+        'puppet',
+        'rest',
+        'sass',
+        'scss',
+        'smarty',
+        'sql',
+        'twig',
+        'vim',
+        'yaml'
+    ];
+
+gulp.task('scripts-home', function () {
+    return gulp.src([
+            'node_modules/imagesloaded/imagesloaded.pkgd.js',
+            'node_modules/isotope-layout/dist/isotope.pkgd.min.js',
+            'js/home/portfolio.js',
+            'js/home/jquery.hoverex.min.js'
+        ])
+        .pipe(concat({path: 'home.js'}))
+        .pipe(uglify({mangle: false}))
+        .pipe(gulp.dest('../public/js/'));
+});
+
+gulp.task('scripts', function () {
+    var prismComponents = [];
+    for (var component in prism) {
+        prismComponents[component] = 'node_modules/prismjs/components/prism-' + prism[component] + '.min.js';
+    }
+
+    return gulp.src(prismComponents.concat([
+            'node_modules/prismjs/plugins/normalize-whitespace/prism-normalize-whitespace.min.js'
+        ]))
+        .pipe(concat({path: 'scripts.js'}))
+        .pipe(uglify({mangle: false}))
+        .pipe(gulp.dest('../public/js/'));
+});
+
+gulp.task('styles', ['scripts-home'], function () {
+    return gulp.src('sass/*.scss')
+        .pipe(cssimport({filter: /^..\/node_modules\//gi}))
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('../public/css/'));
+});
+
+gulp.task('revision', ['styles', 'scripts'], function () {
+    gulp.src([
+            '../public/css/manual.css',
+            '../public/css/styles.css',
+            '../public/js/home.js',
+            '../public/js/scripts.js'
+        ], {base: '../public'})
+        .pipe(rev())
+        .pipe(revOriginDel())
+        .pipe(gulp.dest('../public/'))
+        .pipe(rev.manifest())
+        .pipe(revDel({
+            oldManifest: 'rev-manifest.json',
+            dest: '../public/',
+            force: true
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['revision']);
