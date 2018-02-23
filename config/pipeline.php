ead0381128cec950878863bb8af54025221e8653
webimpress <contact@webimpress.com>
2018-02-23 08:50:37 +0000

    Fixed indents in pipeline and routes config files

--- config/pipeline.php
+++ config/pipeline.php
@@ -1,26 +1,26 @@
 <?php
 
 declare(strict_types=1);
+
 /**
  * Expressive middleware pipeline
  */
 
-/** @var \Zend\Expressive\Application $app */
 return function (
     \Zend\Expressive\Application $app,
     \Zend\Expressive\MiddlewareFactory $factory,
     \Psr\Container\ContainerInterface $container
 ) : void {
-$app->pipe(\Zend\Stratigility\Middleware\OriginalMessages::class);
-$app->pipe(\Zend\Stratigility\Middleware\ErrorHandler::class);
-$app->pipe(\App\Action\StripTrailingSlashMiddleware::class);
-$app->pipe(\App\Action\Redirects::class);
-$app->pipe(\Zend\Expressive\Helper\ServerUrlMiddleware::class);
-$app->pipe(\Zend\Expressive\Router\Middleware\PathBasedRoutingMiddleware::class);
-$app->pipe(\Zend\Expressive\Router\Middleware\MethodNotAllowedMiddleware::class);
-$app->pipe(\Zend\Expressive\Router\Middleware\ImplicitHeadMiddleware::class);
-$app->pipe(\Zend\Expressive\Router\Middleware\ImplicitOptionsMiddleware::class);
-$app->pipe(\Zend\Expressive\Helper\UrlHelperMiddleware::class);
-$app->pipe(\Zend\Expressive\Router\Middleware\DispatchMiddleware::class);
-$app->pipe(\Zend\Expressive\Handler\NotFoundHandler::class);
+    $app->pipe(\Zend\Stratigility\Middleware\OriginalMessages::class);
+    $app->pipe(\Zend\Stratigility\Middleware\ErrorHandler::class);
+    $app->pipe(\App\Action\StripTrailingSlashMiddleware::class);
+    $app->pipe(\App\Action\Redirects::class);
+    $app->pipe(\Zend\Expressive\Helper\ServerUrlMiddleware::class);
+    $app->pipe(\Zend\Expressive\Router\Middleware\PathBasedRoutingMiddleware::class);
+    $app->pipe(\Zend\Expressive\Router\Middleware\MethodNotAllowedMiddleware::class);
+    $app->pipe(\Zend\Expressive\Router\Middleware\ImplicitHeadMiddleware::class);
+    $app->pipe(\Zend\Expressive\Router\Middleware\ImplicitOptionsMiddleware::class);
+    $app->pipe(\Zend\Expressive\Helper\UrlHelperMiddleware::class);
+    $app->pipe(\Zend\Expressive\Router\Middleware\DispatchMiddleware::class);
+    $app->pipe(\Zend\Expressive\Handler\NotFoundHandler::class);
 };
--- config/routes.php
+++ config/routes.php
@@ -1,32 +1,32 @@
 <?php
 
 declare(strict_types=1);
+
 /**
  * Expressive routed middleware
  */
 
-/** @var \Zend\Expressive\Application $app */
 return function (
     \Zend\Expressive\Application $app,
     \Zend\Expressive\MiddlewareFactory $factory,
     \Psr\Container\ContainerInterface $container
 ) : void {
-$app->get('/api/zf-version', \App\Action\ApiAction::class, 'api-zf-version');
-$app->get('/', \App\Action\HomePageAction::class, 'home');
-$app->get('/blog[/:file]', \App\Action\BlogAction::class, 'blog');
-$app->get('/about[/faq]', \App\Action\AboutAction::class, 'about');
-$app->get('/license', \App\Action\AboutAction::class, 'license');
-$app->get('/long-term-support', \App\Action\AboutAction::class, 'long-term-support');
-$app->get('/security[/:action]', \App\Action\SecurityAction::class, 'security');
-$app->get('/security/advisory/:advisory', \App\Action\AdvisoryAction::class, 'advisory');
-$app->get('/changelog[/:changelog]', \App\Action\ChangelogAction::class, 'changelog');
-$app->get('/issues[/:type[/:issue]]', \App\Action\IssueAction::class, 'issue');
-$app->get('/manual/:version/:lang/:page[/:subpage]', \App\Action\ManualAction::class, 'manual');
-$app->post('/switch-manual', \App\Action\SwitchManualAction::class, 'switch-manual');
-$app->get('/learn[/:page]', \App\Action\LearnAction::class, 'learn');
-$app->get('/docs[/api/:ver]', \App\Action\DocsAction::class, 'docs');
-$app->get('/downloads[/:page]', \App\Action\InstallAction::class, 'install');
-$app->get('/participate[/:page]', \App\Action\ParticipateAction::class, 'participate');
-$app->get('/status', \App\Action\StatusAction::class, 'status');
-$app->get('/stats', \App\Action\StatisticsAction::class, 'statistics');
+    $app->get('/api/zf-version', \App\Action\ApiAction::class, 'api-zf-version');
+    $app->get('/', \App\Action\HomePageAction::class, 'home');
+    $app->get('/blog[/:file]', \App\Action\BlogAction::class, 'blog');
+    $app->get('/about[/faq]', \App\Action\AboutAction::class, 'about');
+    $app->get('/license', \App\Action\AboutAction::class, 'license');
+    $app->get('/long-term-support', \App\Action\AboutAction::class, 'long-term-support');
+    $app->get('/security[/:action]', \App\Action\SecurityAction::class, 'security');
+    $app->get('/security/advisory/:advisory', \App\Action\AdvisoryAction::class, 'advisory');
+    $app->get('/changelog[/:changelog]', \App\Action\ChangelogAction::class, 'changelog');
+    $app->get('/issues[/:type[/:issue]]', \App\Action\IssueAction::class, 'issue');
+    $app->get('/manual/:version/:lang/:page[/:subpage]', \App\Action\ManualAction::class, 'manual');
+    $app->post('/switch-manual', \App\Action\SwitchManualAction::class, 'switch-manual');
+    $app->get('/learn[/:page]', \App\Action\LearnAction::class, 'learn');
+    $app->get('/docs[/api/:ver]', \App\Action\DocsAction::class, 'docs');
+    $app->get('/downloads[/:page]', \App\Action\InstallAction::class, 'install');
+    $app->get('/participate[/:page]', \App\Action\ParticipateAction::class, 'participate');
+    $app->get('/status', \App\Action\StatusAction::class, 'status');
+    $app->get('/stats', \App\Action\StatisticsAction::class, 'statistics');
 };
