72b9e13dddad7b72482745881e5c0ac1d07b7504
Matthew Weier O'Phinney <matthew@zend.com>
2019-02-14 14:27:18 -0600

    fix: ensure deploy payload is correctly formed
    This patch creates a variable with the curl deployment payload, which
    allowed me to test that it was correctly formed.

--- .deploy/travis_deploy.sh
+++ .deploy/travis_deploy.sh
@@ -2,10 +2,11 @@
 TOKEN=$1
 SITE=$2
 COMMIT=$3
+DATA='{"site":"'"${SITE}"'","commit":"'"${COMMIT}"'"}'
 
 curl -k \
     --request POST \
     --header "Authorization: Bearer ${TOKEN}" \
     --header "Content-Type: application/x-zfweb-deploy+json" \
-    --data '"site":"'"${SITE}"'","commit":"'"${COMMIT}"'"}' \
+    --data $DATA \
     https://zf-node1.zend.com
